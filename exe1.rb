ebaba4a918ab5f8aa501ac30245d87d6ef2baa3e
AndrewBell777 <andy-b-boi@hotmail.com>
2015-05-09 11:25:23 +0100

    first_project
    Ruby Code.

---
+++ exe1.rb
@@ -0,0 +1,17 @@
+BEGIN {
+	puts("Booting ILLUMiNATE v0.0.1")
+}
+
+print("Please Identify Yourself: ")
+Name = gets
+gets.Andrew = true
+if true
+	puts("Hello, " + Name)
+	puts("My name is ILLUMiNATE")	
+	time1 = Time.new
+	puts("The current time is : " + time1.inspect)
+	puts("What would you like to do now?")
+elsif false
+	puts("AUTHENTICATION ERROR! Log in failed!")
+	
+end
